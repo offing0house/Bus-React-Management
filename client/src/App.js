7bc3df9f5bec9d49a88bf55d2145cf87784e1439
[Github <[Github>
2022-02-11 23:54:25 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -9,48 +9,50 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+
 
 const styles = theme => ({
   root:{
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto"
   },
   table: {
     minWidth:1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 })
 
-const customers=[
-{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name' : 'a1',
-  'birthday' : 'b1',
-  'gender' : 'c1',
-  'job' : 'd1'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name' : 'a2',
-  'birthday' : 'b2',
-  'gender' : 'c2',
-  'job' : 'd2'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name' : 'a3',
-  'birthday' : 'b3',
-  'gender' : 'c3',
-  'job' : 'd3'
-}
-]
-
-
 
 class App extends Component {
+
+  state = {
+    customers: "",
+    completed: 0
+  }
+
+  componentDidMount(){
+    this.timer = setInterval(this.progress,20);
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({completed: completed+1});
+  }
+
   render(){
     const { classes } = this.props;
     return (
@@ -67,12 +69,17 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name} gender={c.gender} job={c.job} />);})}
+            {this.state.customers ? this.state.customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name} gender={c.gender} job={c.job} />);
+            }) : 
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+              </TableCell>
+            </TableRow>
+            }
+           
           </TableBody>
-        
         </Table>
-        
-
       </Paper>
     );
   }
--- server.js
+++ server.js
@@ -6,8 +6,37 @@ const port = process.env.PORT | 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello',(req,res)=>{
-    res.send({message: 'Hello Express!'});
-});
+// app.get('/api/hello',(req,res)=>{
+//     res.send({message: 'Hello Express!'});
+// });
+
+app.get('/api/customers',(req,res) => {
+    res.send([
+            {
+              'id': 1,
+              'image': 'https://placeimg.com/64/64/1',
+              'name' : 'a1',
+              'birthday' : 'b1',
+              'gender' : 'c1',
+              'job' : 'd1'
+            },
+            {
+              'id': 2,
+              'image': 'https://placeimg.com/64/64/2',
+              'name' : 'a2',
+              'birthday' : 'b2',
+              'gender' : 'c2',
+              'job' : 'd2'
+            },
+            {
+              'id': 3,
+              'image': 'https://placeimg.com/64/64/3',
+              'name' : 'a3',
+              'birthday' : 'b3',
+              'gender' : 'c3',
+              'job' : 'd3'
+            }
+    ]);
+})
 
 app.listen(port,() => console.log(`Listening on port ${port}`));
