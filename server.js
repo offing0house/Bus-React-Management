4841adc1e5a2cdd4750be06e30b5c15f0251a951
[Github <[Github>
2022-02-12 09:54:19 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,37 +7,25 @@ const port = process.env.PORT | 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/api/hello',(req,res)=>{
-//     res.send({message: 'Hello Express!'});
-// });
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
 
 app.get('/api/customers',(req,res) => {
-    res.send([
-            {
-              'id': 1,
-              'image': 'https://placeimg.com/64/64/1',
-              'name' : 'a1',
-              'birthday' : 'b1',
-              'gender' : 'c1',
-              'job' : 'd1'
-            },
-            {
-              'id': 2,
-              'image': 'https://placeimg.com/64/64/2',
-              'name' : 'a2',
-              'birthday' : 'b2',
-              'gender' : 'c2',
-              'job' : 'd2'
-            },
-            {
-              'id': 3,
-              'image': 'https://placeimg.com/64/64/3',
-              'name' : 'a3',
-              'birthday' : 'b3',
-              'gender' : 'c3',
-              'job' : 'd3'
-            }
-    ]);
-})
-
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err,rows,fields) => {
+        res.send(rows);
+      }
+    );
+});
 app.listen(port,() => console.log(`Listening on port ${port}`));
