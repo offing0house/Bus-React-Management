277cbef8fb5bead4e1a170ac49884bf71f67cbb4
[Github <[Github>
2022-03-08 15:29:21 +0900

    s

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const multer = require('multer');
 const upload = multer({dest: './upload'})
 
 
-app.get('http://ec2-35-78-68-96.ap-northeast-1.compute.amazonaws.com/getLowArrInfoByStId/',(req,res) => {
+app.get('/getLowArrInfoByStId/',(req,res) => {
   const query = url.parse(req.url,true).query;
   const stId = query.stId;
   const OPEN_API_KEY = '9bnaou0550%2BAePv%2BKJ5P03FS0nJlJqhAenEHOxpImpaW4XrlVsZ0g2Xynfg1el0zE8cE6OoWSQ2Igpu5%2F0Scyg%3D%3D';
@@ -57,7 +57,7 @@ app.get('http://ec2-35-78-68-96.ap-northeast-1.compute.amazonaws.com/getLowArrIn
 
 
 
-app.get('http://ec2-35-78-68-96.ap-northeast-1.compute.amazonaws.com/api/customers',(req,res) => {
+app.get('/api/customers',(req,res) => {
     connection.query(
       "SELECT * FROM CUSTOMER WHERE isDeleted = 1",
       (err,rows,fields) => {
@@ -67,7 +67,7 @@ app.get('http://ec2-35-78-68-96.ap-northeast-1.compute.amazonaws.com/api/custome
 });
 
 app.use('/image',express.static('./upload'));
-app.post('http://ec2-35-78-68-96.ap-northeast-1.compute.amazonaws.com/api/customers', upload.single('image'), (req,res) => {
+app.post('/api/customers', upload.single('image'), (req,res) => {
   let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), ?)';
   let image = req.body.image;
   let name = req.body.name;
@@ -85,7 +85,7 @@ app.post('http://ec2-35-78-68-96.ap-northeast-1.compute.amazonaws.com/api/custom
 });
 
 
-app.delete('http://ec2-35-78-68-96.ap-northeast-1.compute.amazonaws.com/api/customers/:id',(req,res)=>{
+app.delete('/api/customers/:id',(req,res)=>{
   let sql = 'DELETE FROM CUSTOMER WHERE id = ?';
   let params = [req.params.id];
   
